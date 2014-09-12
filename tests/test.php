746a2a385929e7d3b4fb483a0d81783f85c34253
stevepeak <steve@stevepeak.net>
2014-09-12 12:19:11 -0400

    test clover method

--- tests/test.php
+++ tests/test.php
@@ -1,35 +1,18 @@
 <?php
-    require __DIR__ .'/../vendor/autoload.php';
-
-    xdebug_start_code_coverage();
-    
-    use Codecov\Coverage;
 
     function a($a) {
         return $a +1;
     }
 
-    function b($count) {
-        if (1) {
-            return a(2);
-        } else {
-            return false;
-        }
+    function b() {
+        return false;
     }
 
-    b(6);
-
-    class CodecovTest extends PHPUnit_Framework_TestCase
+    class Test extends PHPUnit_Framework_TestCase
     {
-        public function testSend()
+        public function testExample()
         {
-            $result = Codecov\Coverage::send();
-        }
-        public function testCoverageArray()
-        {
-            $coverage = xdebug_get_code_coverage();
-            $this->assertTrue();
-
+            $this->assertTrue(a(6) == 7);
         }
     }
 ?>  
