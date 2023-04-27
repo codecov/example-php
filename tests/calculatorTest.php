b8952c342ec521e1cecd942ff94067946faf55a4
cwengazc <92723005+cwengazc@users.noreply.github.com>
2023-04-27 12:04:31 +0200

    Update calculatorTest.php

--- tests/calculatorTest.php
+++ tests/calculatorTest.php
@@ -20,15 +20,15 @@ class calculatorTest extends PHPUnit\Framework\TestCase
     /**
      * @covers Calculator\BasicCalculator
     */
-//     public function testSubtract()
-//     {
-//         $this->assertEquals(BasicCalculator::subtract(1, 2), -1.0);
-//         $this->assertEquals(BasicCalculator::subtract(2, 1), 1.0);
-//         $this->assertEquals(BasicCalculator::subtract(1.0, 2.0), -1.0);
-//         $this->assertEquals(BasicCalculator::subtract(0, 2.0), -2.0);
-//         $this->assertEquals(BasicCalculator::subtract(2.0, 0), 2.0);
-//         $this->assertEquals(BasicCalculator::subtract(-4, 2.0), -6.0);
-//     }
+    public function testSubtract()
+    {
+        $this->assertEquals(BasicCalculator::subtract(1, 2), -1.0);
+        $this->assertEquals(BasicCalculator::subtract(2, 1), 1.0);
+        $this->assertEquals(BasicCalculator::subtract(1.0, 2.0), -1.0);
+        $this->assertEquals(BasicCalculator::subtract(0, 2.0), -2.0);
+        $this->assertEquals(BasicCalculator::subtract(2.0, 0), 2.0);
+        $this->assertEquals(BasicCalculator::subtract(-4, 2.0), -6.0);
+    }
 
     /**
      * @covers Calculator\BasicCalculator
